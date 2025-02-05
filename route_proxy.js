cbef418cd7bc38adb1ecd31cd847e3d36b40ead0
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2025-02-05 11:28:08 -0300

    feat: update route proxy server

---
+++ route_proxy.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var httpProxy = require('http-proxy');
+var HttpProxyRules = require('http-proxy-rules');
+
+const rules = require('./route-proxy.json');
+
+var proxyRules = new HttpProxyRules({ rules: rules });
+
+var proxy = httpProxy.createProxy();
+
+const httpServer = http.createServer(function(req, res) {
+    var target = proxyRules.match(req);
+    if (target) {
+        return proxy.web(req, res, { target: target });
+    }
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 not found');
+});
+
+httpServer.listen(80, function () {
+    console.log("Route Proxy is running");
+});
