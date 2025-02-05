98e8795dfd818f14b72d98559a5f6919b115cf84
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2025-02-05 11:31:56 -0300

    feat: update clean script

--- clean.sh
+++ clean.sh
@@ -2,13 +2,7 @@ read -r -p "This action will remove all pm2 services and instance configs, are y
 case "$response" in
     [yY][eE][sS]|[yY])
         pm2 delete all
-        rm ./configs/*
         rm -rf ./logs/*
-        rm -rf ./cloudcode/*
-        rm ./appServers.json
-        rm ./count.json
-        rm ./route-proxy.json
-        rm ./dashboards.json
         ;;
     *)
         ;;
