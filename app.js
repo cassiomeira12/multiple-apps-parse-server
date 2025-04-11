d061dcdbedb56e0329d6989888ae8c326d57eac5
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2025-04-11 17:01:59 -0300

    fix: irgnore /parse/users/me endpoint

--- app.js
+++ app.js
@@ -134,7 +134,8 @@ if (allowedOrigins.length > 0) {
 app.all('*', (req, res, next) => {
   const origin = req.get('origin');
   const url = req.originalUrl;
-  if (origin === undefined && url !== '/parse/health') {
+  if (origin !== undefined) return;
+  if (url !== '/parse/health' || url !== '/parse/users/me') {
     if (req.header('X-Parse-Client-Key') !== config.clientKey) {
       return res.status(403).send({
         'error': 'unauthorized',
