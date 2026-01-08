fb65b4642c74e527d7046c69e84836e9a0e8c42c
Cássio M. Silva (Dev) <dev.cassiomsilva@gmail.com>
2026-01-08 16:33:47 -0300

    refactor: disable session expire

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ var configParse = {
   "enableAnonymousUsers": false,
   "allowClientClassCreation": false,
   "allowExpiredAuthDataToken": false,
-  // "expireInactiveSessions": true,
+  "expireInactiveSessions": false, // Sessions has no expiration date
   // "sessionLength": 3600 * 24 * 15, // Session expires in 15 days
   "jsonLogs": true,
   "logsFolder": "./logs/" + config.appName,
